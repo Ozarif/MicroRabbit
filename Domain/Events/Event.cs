4df21145e67c78aacfb5f3e1f358fa3071da6f85
Oussama Zarif <ozarif@gmail.com>
2020-07-11 19:27:13 +0300

    first commit

---
+++ Domain/Events/Event.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public class Event
+    {
+        public DateTime TimeStamp {get; protected set;}
+
+        protected Event()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
